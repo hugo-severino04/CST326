c411bfa43e005678e02e6c25978ba21db987dce6
hugo-severino04 <hugo.severino2014@gmail.com>
2025-02-26 21:27:21 -0800

    finish

---
+++ Week5/PlatformerV2/Assets/Platformer/Scripts/character_Movement.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class character_Movement : MonoBehaviour
+{
+    public float acceleration = 3f;
+    public float maxSpeed = 10f;
+    public float jumpImpulse = 15f;
+    public float jumpBoostForce = 5.7f;
+    Rigidbody rb;
+    
+    [Header("Debug Stuff")]
+    public bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAmount = Input.GetAxis("Horizontal");
+        rb.linearVelocity += Vector3.right * (horizontalAmount * Time.deltaTime * acceleration);
+        
+        float horizontalSpeed = rb.linearVelocity.x;
+        horizontalSpeed = Mathf.Clamp(horizontalSpeed, -maxSpeed, maxSpeed);
+        
+        Vector3 newVelocity = rb.linearVelocity;
+        newVelocity.x = horizontalSpeed;
+        rb.linearVelocity = newVelocity;
+        
+        // should also clamp vertical 
+        
+        // test if the character is on the ground 
+        Collider c = GetComponent<Collider>(); 
+        float castDistance = c.bounds.extents.y + 0.01f;
+        Vector3 startPoint = transform.position;
+        
+        isGrounded = Physics.Raycast(startPoint, Vector3.down, castDistance);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            // apply an impulse force upwards 
+            rb.AddForce(Vector3.up * jumpImpulse, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey(KeyCode.Space) && !isGrounded)
+        {
+            if (rb.linearVelocity.y > 0) // Only apply boost while moving up
+            {
+                rb.AddForce(Vector3.up * jumpBoostForce, ForceMode.Acceleration);
+            }
+        }
+
+        if (horizontalAmount == 0f)
+        {
+            Vector3 decayedVelocity = rb.linearVelocity;
+            decayedVelocity.x *= 1f - Time.deltaTime * 5f;
+            rb.linearVelocity = decayedVelocity;
+        }
+
+        else
+        {
+            float yawRotation = (horizontalAmount > 0f) ? 90f : -90f;
+            Quaternion rotation = Quaternion.Euler(0f, yawRotation, 0f);
+            transform.rotation = rotation;
+        }
+        
+    }
+}
